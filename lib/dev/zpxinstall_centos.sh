9b28c1611f49b2138f9645e2f6fadf24350623bd
RusTus <kempo19b@yahoo.com>
2011-06-06 11:46:26 +0000

    Updated CentOS install script to include mod_bw and suhosin.  Also added user configuration to automatically generate the CP vhost, and zcnf.php credentials.

--- lib/dev/zpxinstall_centos.sh
+++ lib/dev/zpxinstall_centos.sh
@@ -13,17 +13,28 @@ proftpd_config=/etc/proftpd.conf
 
 clear
 echo "#########################################################"
-echo "# ZPANEL ONLINE INSTALLER (for CentOS)                  #"
-echo "#########################################################"
+echo "# ZPanel Installation Package for CentOS Linux          #"
+echo "# --------------------------------------------          #"
+echo "# Package maintainer: Bobby Allen (ballen@zpanelcp.com) #"
+echo "# Last updated:       04/06/2011                        #"
+echo "# Website:            http://www.zpanelcp.com           #"
+echo "########################################################"
 echo ""
-echo "Welcome to the online installer for ZPanel, this will download the latest source over SVN and install it for you."
-echo "It will attempt to download and install all the required software too!"
-echo "Any bugs should be logged here: http://bugs.zpanelcp.com"
-echo "Thanks,"
-echo "Bobby (ballen@zpanelcp.com)"
+echo "Welcome to the online installer for ZPanel, this will download the required software and install ZPanel."
+echo ""
+echo "This install script is designed to be used on freshly installed servers or workstations"
+echo "due to the nature of the software and amount of system changes it makes we recommend that"
+echo "if you want to uninstall ZPanel that you re-install your OS."
+echo ""
+echo "We also recommend that ZPanel is installed on a dedicated server for security reasons!"
+echo ""
+echo "Press ENTER to continue with the installation... (or CTRL+C to quit)"
+read continue
 
 # Install the required development enviroment packages...
-sudo yum install httpd php53 php53-devel php53-gd php53-mbstring php53-imap php53-mysql php53-xml php53-xmlrpc curl curl-devel perl-libwww-perl libxml2 libxml2-devel mysql-server subversion zip webalizer gcc gcc-c++
+sudo yum update
+sudo yum install httpd php53 php53-devel php53-gd php53-mbstring php53-imap php53-mysql php53-xml php53-xmlrpc curl curl-devel perl-libwww-perl libxml2 libxml2-devel mysql-server subversion zip webalizer gcc gcc-c++ httpd-devel.i386 postfix dovecot system-switch-mail
+#sudo chkconfig --levels 235 sendmail off; /etc/init.d/sendmail stop; yum -y remove sendmail
 sudo yum remove vsftpd
 
 # We have to install ProFTPd Manually as CentOS does not have a package for it..
@@ -43,6 +54,29 @@ mv proftpd_centos55.txt proftpd
 mv proftpd /etc/init.d/proftpd
 chmod 755 /etc/init.d/proftpd
 
+# We have to install Mod_BW Manually as CentOS does not have a package for it..
+cd /tmp/
+wget http://ivn.cl/files/source/mod_bw-0.92.tgz
+tar -zxvf mod_bw-0.92.tgz
+apxs -i -a -c mod_bw.c
+rm -fr mod_bw*
+cd ~
+
+# We have to install suhosin Manually as CentOS does not have a package for it...
+cd /tmp/
+wget http://download.suhosin.org/suhosin-0.9.29.tgz
+tar xvfz suhosin-0.9.29.tgz
+cd suhosin-0.9.29
+phpize
+./configure
+make
+make install
+touch /etc/php.d/suhosin.ini
+chmod 755 /etc/php.d/suhosin.ini
+echo "extension=suhosin.so" > /etc/php.d/suhosin.ini
+cd ..
+rm -fr suhosin-0.9.29*
+cd ~
 
 
 # Add 'include' to the Apache configuration file..
@@ -98,17 +132,102 @@ echo "0,30 * * * * php /etc/zpanel/daemon.php" >> /etc/crontab
 # Set permissions so Apache can create cronjobs!
 sudo chmod 777 /etc/crontab
 
+clear
 echo "Will now attempt to create and insert the ZPanel core database into MySQL, please enter the MySQL root password when asked..."
-mysql -uroot -p < /etc/zpanel/lib/dev/zpanel_core.sql
+echo "Enter MySQL root password:"
+read password
+mysql -uroot -p${password} < /etc/zpanel/lib/dev/zpanel_core.sql
 echo "Will now attempt to create and insert the ZPanel postfix database into MySQL, please enter the MySQL root password again when asked..."
-mysql -uroot -p < /etc/zpanel/lib/dev/zpanel_postfix.sql
+mysql -uroot -p${password} < /etc/zpanel/lib/dev/zpanel_postfix.sql
 
-echo "=================================="
-echo "Enviroment has been prepared..."
-echo " Just a few more steps..."
+echo "<?php" > /etc/zpanel/conf/zcnf.php
+echo "" >> /etc/zpanel/conf/zcnf.php
+echo "/**" >> /etc/zpanel/conf/zcnf.php
+echo " *" >> /etc/zpanel/conf/zcnf.php
+echo " * ZPanel - A Cross-Platform Open-Source Web Hosting Control panel." >> /etc/zpanel/conf/zcnf.php
+echo " * " >> /etc/zpanel/conf/zcnf.php
+echo " * @package ZPanel" >> /etc/zpanel/conf/zcnf.php
+echo " * @version $Id$" >> /etc/zpanel/conf/zcnf.php
+echo " * @author Bobby Allen - ballen@zpanelcp.com" >> /etc/zpanel/conf/zcnf.php
+echo " * @copyright (c) 2008-2011 ZPanel Group - http://www.zpanelcp.com/" >> /etc/zpanel/conf/zcnf.php
+echo " * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License v3" >> /etc/zpanel/conf/zcnf.php
+echo " *" >> /etc/zpanel/conf/zcnf.php
+echo " * This program (ZPanel) is free software: you can redistribute it and/or modify" >> /etc/zpanel/conf/zcnf.php
+echo " * it under the terms of the GNU General Public License as published by" >> /etc/zpanel/conf/zcnf.php
+echo " * the Free Software Foundation, either version 3 of the License, or" >> /etc/zpanel/conf/zcnf.php
+echo " * (at your option) any later version." >> /etc/zpanel/conf/zcnf.php
+echo " *" >> /etc/zpanel/conf/zcnf.php
+echo " * This program is distributed in the hope that it will be useful," >> /etc/zpanel/conf/zcnf.php
+echo " * but WITHOUT ANY WARRANTY; without even the implied warranty of" >> /etc/zpanel/conf/zcnf.php
+echo " * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" >> /etc/zpanel/conf/zcnf.php
+echo " * GNU General Public License for more details." >> /etc/zpanel/conf/zcnf.php
+echo " *" >> /etc/zpanel/conf/zcnf.php
+echo " * You should have received a copy of the GNU General Public License" >> /etc/zpanel/conf/zcnf.php
+echo " * along with this program.  If not, see <http://www.gnu.org/licenses/>." >> /etc/zpanel/conf/zcnf.php
+echo " *" >> /etc/zpanel/conf/zcnf.php
+echo " */" >> /etc/zpanel/conf/zcnf.php
+echo "\$z_db_host = \"localhost\";" >> /etc/zpanel/conf/zcnf.php
+echo "\$z_db_name = \"zpanel_core\";" >> /etc/zpanel/conf/zcnf.php
+echo "\$z_db_user = \"root\";" >> /etc/zpanel/conf/zcnf.php
+echo "\$z_db_pass = \"${password}\";" >> /etc/zpanel/conf/zcnf.php
+echo "\$zdb = @mysql_pconnect(\$z_db_host, \$z_db_user, \$z_db_pass) or trigger_error('ZPanel Stack Error :: Unable to connect to ZPanel Database Server (' . \$z_db_host . ').');" >> /etc/zpanel/conf/zcnf.php
+echo "?>" >> /etc/zpanel/conf/zcnf.php
+echo "  ^ Done"
+
+# Setup the default virtual host for the control panel
+clear
+echo "ENTER THE DOMAIN/SUBDOMAIN THAT WILL HOST ZPANEL EG. 'CONTROL.YOURDOMAIN.COM'"
 echo " "
-echo "   1) Open http://localhost/phpmyadmin/ and login as 'root'."
-echo "   3) Add a MySQL user named 'zpanel' and password of 'zpanel' if you choose another account (recommended) you should edit the MySQL username and password in /etc/zpanel/conf/zcnf.php"
-echo ""
-echo ""
+read domain
+echo "# ZPanel Apache Master VHOST file." > /etc/zpanel/conf/httpd-vhosts.conf
+echo "# Written by Bobby Allen, 15/05/2011" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "#" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "# DO NOT EDIT THIS FILE MANUALLY - USE ZPANEL TO ADD AND REMOVE VHOSTS!" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo " " >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "NameVirtualHost *:80" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo " " >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	# Configuration for ZPanel control panel." >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	<VirtualHost *:80>" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	ServerAdmin zadmin@${domain}" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	DocumentRoot \"/etc/zpanel\"" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	ServerName ${domain}" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	ServerAlias *.${domain}" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	AddType application/x-httpd-php .php" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	<Directory \"/etc/zpanel\">" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	Options FollowSymLinks" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	AllowOverride None" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	Order allow,deny" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	Allow from all" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	</Directory>" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "  	<location /modbw>" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "    	SetHandler modbw-handler" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo " 	</location>" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo " " >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	Header add X-Hello \"time %D\"" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo " " >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	</VirtualHost>" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo " " >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	########################################################" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	# ZPanel generated VHOST configurations below.....     #" >> /etc/zpanel/conf/httpd-vhosts.conf
+echo "	########################################################" >> /etc/zpanel/conf/httpd-vhosts.conf
+
+service httpd restart
+
+echo "127.0.0.1			${domain}">> /etc/hosts
+
+clear
+echo "===================================================="
+echo "ZPanel has now been installed!"
+echo " "
+echo "IMPORTANT, ENSURE YOU MAKE NOTE OF THESE SETTINGS!!!"
+echo " "
+echo "      NEW MYSQL ROOT ACCOUNT"
+echo "      =============================================="
+echo "      PASSWORD: ${password}"
+echo " "
+echo "      ZPANEL ADMIN ACCOUNT LOGIN"
+echo "      =============================================="
+echo "      CONTROL PANEL URL: http://${domain}"
+echo "      USERNAME: zadmin"
+echo "      PASSWORD: zadmin"
 
