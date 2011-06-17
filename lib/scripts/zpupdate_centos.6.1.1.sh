0e69f75cf3f3b9b5c7883406b5a1af261b429ae3
bobsta63 <bobbyallen.uk@gmail.com>
2011-06-17 19:40:28 +0000

    Added lib/scripts folder as a standard directory for upgrade shell/batch files which are required to be run as part of upgrades etc.

---
+++ lib/scripts/zpupdate_centos.6.1.1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# CentOS Linux Update Script for Zpanel 6.1.1
+# Script written by Bobby Allen (ballen@zpanel.co.uk) 14/05/2011
+#
+#
+
+# Remove old zdaemon cron
+rm -rf /etc/cron.d/zdaemon
+# Add updated cron task to run deamon every 60 mins...
+touch /etc/cron.d/zdaemon
+echo "0 * * * * root /usr/bin/php -q /etc/zpanel/daemon.php >> /dev/null 2>&1" >> /etc/cron.d/zdaemon
+# Permissions must be 644 or cron will not run!
+sudo chmod 644 /etc/cron.d/zdaemon
+service cron restart
+
+sudo chown root /etc/zpanel/bin/zsudo
+sudo chmod 4777 /etc/zpanel/bin/zsudo
+
+# Create ZPanel Cron file and set permissions
+touch /var/spool/cron/apache
+sudo chgrp apache /var/spool/cron
+sudo chmod 777 /var/spool/cron
+sudo chown apache /var/spool/cron/root
+sudo chmod 644 /var/spool/cron/root
+
+echo "#################################################################################" > /var/spool/cron/apache
+echo "# CRONTAB FOR ZPANEL CRON MANAGER MODULE                                        #" >> /var/spool/cron/apache
+echo "# Module Developed by Bobby Allen, 17/12/2009                                   #" >> /var/spool/cron/apache
+echo "#                                                                               #" >> /var/spool/cron/apache
+echo "#################################################################################" >> /var/spool/cron/apache
+echo "# WE DO NOT RECOMMEND YOU MODIFY THIS FILE DIRECTLY, PLEASE USE ZPANEL INSTEAD! #" >> /var/spool/cron/apache
+echo "#################################################################################" >> /var/spool/cron/apache
+echo "# DO NOT MANUALLY REMOVE ANY OF THE CRON ENTRIES FROM THIS FILE, USE ZPANEL     #" >> /var/spool/cron/apache
+echo "# INSTEAD! THE ABOVE ENTRIES ARE USED FOR ZPANEL TASKS, DO NOT REMOVE THEM!     #" >> /var/spool/cron/apache
+echo "#################################################################################" >> /var/spool/cron/apache
+
+/etc/zpanel/lib/dev/setso --set -q cron_file /var/spool/cron/apache
+
+# Remove the old version_checker module...
+rm -rf /etc/zpanel/modules/admin/version_checker/
---
+++ lib/scripts/zpupgrade_ubuntu.6.1.1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Ubuntu Linux Update Script for Zpanel 6.1.1
+# Script written by Bobby Allen (ballen@zpanel.co.uk) 14/05/2011
+#
+#
+
+# Remove old zdaemon cron
+rm -rf /etc/cron.d/zdaemon
+# Add updated cron task to run deamon every 60 mins...
+touch /etc/cron.d/zdaemon
+echo "0 * * * * root /usr/bin/php -q /etc/zpanel/daemon.php >> /dev/null 2>&1" >> /etc/cron.d/zdaemon
+# Permissions must be 644 or cron will not run!
+sudo chmod 644 /etc/cron.d/zdaemon
+service cron restart
+
+sudo chown root /etc/zpanel/bin/zsudo
+sudo chmod 4777 /etc/zpanel/bin/zsudo
+
+# Create ZPanel Cron file and set permissions
+touch /var/spool/cron/crontabs/www-data
+sudo chmod 777 /var/spool/cron/crontabs
+sudo chown www-data /var/spool/cron/crontabs/www-data
+sudo chmod 644 /var/spool/cron/crontabs/www-data
+
+echo "#################################################################################" > /var/spool/cron/crontabs/www-data
+echo "# CRONTAB FOR ZPANEL CRON MANAGER MODULE                                        #" >> /var/spool/cron/crontabs/www-data
+echo "# Module Developed by Bobby Allen, 17/12/2009                                   #" >> /var/spool/cron/crontabs/www-data
+echo "#                                                                               #" >> /var/spool/cron/crontabs/www-data
+echo "#################################################################################" >> /var/spool/cron/crontabs/www-data
+echo "# WE DO NOT RECOMMEND YOU MODIFY THIS FILE DIRECTLY, PLEASE USE ZPANEL INSTEAD! #" >> /var/spool/cron/crontabs/www-data
+echo "#################################################################################" >> /var/spool/cron/crontabs/www-data
+echo "# DO NOT MANUALLY REMOVE ANY OF THE CRON ENTRIES FROM THIS FILE, USE ZPANEL     #" >> /var/spool/cron/crontabs/www-data
+echo "# INSTEAD! THE ABOVE ENTRIES ARE USED FOR ZPANEL TASKS, DO NOT REMOVE THEM!     #" >> /var/spool/cron/crontabs/www-data
+echo "#################################################################################" >> /var/spool/cron/crontabs/www-data
+
+/etc/zpanel/lib/dev/setso --set -q cron_file /var/spool/cron/crontabs/www-data
+
+# Remove the old version_checker module...
+rm -rf /etc/zpanel/modules/admin/version_checker/
